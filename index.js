3d5f4cbfd7366404ef7862caf83f73798db03bfe
Ko Teeranai <istudko@gmail.com>
2022-05-29 06:05:01 +0700

    refactor index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+import fetch from "isomorphic-fetch";
+import { Coins, LCDClient, MnemonicKey, MsgExecuteContract } from "@terra-money/terra.js";
+
 // ==== Configuration ====
 const mnemonic = ""; // input your 24-word mnemonic key here
 const amount = ""; // input your aUST amount here (ex. 100500000 for 100.500000 aUST)
-
-import fetch from "isomorphic-fetch";
-import { Coins, LCDClient, MnemonicKey, MsgExecuteContract } from "@terra-money/terra.js";
 
 // ==== Initialize client ====
 const gasPrices = await fetch(
@@ -27,17 +27,21 @@ const mk = new MnemonicKey({
 const wallet = lcd.wallet(mk);
 
 // ==== Withdraw all UST from Anchor protocol ====
+const aUSTContract = "terra1hzh9vpxhsk8253se0vv5jj6etdvxu3nv8z07zu";
+const anchorMarketContract = "terra1sepfj7s0aeg5967uxnfk4thzlerrsktkpelm5s";
+const withdrawMsg = "eyJyZWRlZW1fc3RhYmxlIjp7fX0="; // base64 of {"redeem_stable":{}}
 const execute = new MsgExecuteContract(
     wallet.key.accAddress,
-    "terra1hzh9vpxhsk8253se0vv5jj6etdvxu3nv8z07zu",
+    aUSTContract,
     {
         send: {
-            contract: "terra1sepfj7s0aeg5967uxnfk4thzlerrsktkpelm5s",
+            contract: anchorMarketContract,
             amount: amount,
-            msg: "eyJyZWRlZW1fc3RhYmxlIjp7fX0=",
+            msg: withdrawMsg,
         },
     }
 );
 const executeTx = await wallet.createAndSignTx({ msgs: [execute] });
 const executeTxResult = await lcd.tx.broadcast(executeTx);
 console.log(executeTxResult);
+console.log("===== Success =====");
